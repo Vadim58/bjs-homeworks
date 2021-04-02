0c85cc841837df1d04501e6d744b192a86928bc0
Vadim58 <awesome.rdeg@yandex.ru>
2021-04-02 20:12:46 +0300

    first task completed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,11 +1,24 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+    let d = Math.pow(b, 2) - 4 * a * c;
+    if (d === 0) {
+      x.push(-Math.pow(b, 2) / (2 * a));
+      return (x);
+  }
+   else if (d > 0) {
+      let x1 = (-b + Math.sqrt(d)) / (2 * a);
+      let x2 = (-b - Math.sqrt(d)) / (2 * a);
+      x.push(x1, x2);
+      return (x);
 }
+   else {
+    return (x);
+   }
+};
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    // return averageMark;
+    return (averageMark);
 }
 
 function askDrink(name,dateOfBirthday){
