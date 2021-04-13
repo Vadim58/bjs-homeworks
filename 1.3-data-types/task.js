6c014a2b66b7baa2aedabf94741c13c20e6b25e1
Vadim58 <awesome.rdeg@yandex.ru>
2021-04-13 18:46:28 +0300

    Homework correction

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,25 +1,35 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
+ let percentCheck = percent;
+ let contributionCheck = contribution;
+ let amountCheck = amount;
  percent = Number(percent);
  contribution = Number(contribution);
  amount = Number(amount);
-  if (typeof(percent) != "number") {
-  	  return ('Параметр <percent> содержит неправильное значение <' + percent + '>');
+  if (isNaN(percent) === true || percent === undefined || percent <= 0) {
+  	  return (`Параметр <percent> содержит неправильное значение <${percentCheck}>`);
   }
-     if (typeof(contribution) != "number") {
-  	return ('Параметр <contribution> содержит неправильное значение <' + contribution + '>');
+     if (isNaN(contribution) === true || contribution === undefined) {
+  	return (`Параметр <contribution> содержит неправильное значение <${contributionCheck}>`);
   }
-   if (typeof(amount) != "number") {
-  	return ('Параметр <amount> содержит неправильное значение <' + amount + '>');
+   if (isNaN(amount) === true || amount === undefined || amount <= 0) {
+  	return (`Параметр <amount> содержит неправильное значение <${amountCheck}>`);
   }
    let currentDate = new Date();
+   let daymSecValue = 1000 * 60 * 60 * 24;
    let defaultAmount = amount - contribution;
-   let numberOfMonths = (date.getFullYear() - currentDate.getFullYear()) * 12;  
-   let percentRate = percent / 100; 
+   let numberOfMonths = Math.ceil((((date.getTime() - currentDate.getTime()) / daymSecValue) / 365) * 12);
+   console.log(numberOfMonths);
+   let percentRate = percent / (100 * 12);
+   if (numberOfMonths < 1) {
+   	return (`Параметр <date> содержит неправильное значение <${date}>`);
+   } 
+   else { 
+    
    let payment = defaultAmount * (percentRate + percentRate / ((Math.pow((1 + percentRate), numberOfMonths)) - 1));
    let totalAmount = payment * numberOfMonths;
-
-     return totalAmount.toFixed(2);
+   return Number(totalAmount.toFixed(2));
+ }
 }
 
 
@@ -27,8 +37,9 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
 
 function getGreeting(name) {
-      let userName = '';        
-      if (name.length > 0) {
+      let userName = ''; 
+
+      if (name != undefined && name.length > 0) {
       userName = name;
    }
  
@@ -36,7 +47,7 @@ function getGreeting(name) {
       userName = 'Аноним';
  }
 
-      let greeting = 'Привет, мир! Меня зовут ' + userName +'.';
+      let greeting = `Привет, мир! Меня зовут ${userName}.`;
       console.log(greeting);
      
       return greeting;
