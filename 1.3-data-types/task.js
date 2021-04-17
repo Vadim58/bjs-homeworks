a3866fa99fd8a74a0829ab52b18d0f5d602edf28
Vadim58 <awesome.rdeg@yandex.ru>
2021-04-17 10:59:10 +0300

    Fix complete

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -3,32 +3,48 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
  let percentCheck = percent;
  let contributionCheck = contribution;
  let amountCheck = amount;
+ let numberOfMonths;
  percent = Number(percent);
  contribution = Number(contribution);
  amount = Number(amount);
   if (isNaN(percent) === true || percent === undefined || percent <= 0) {
   	  return (`Параметр <percent> содержит неправильное значение <${percentCheck}>`);
   }
-     if (isNaN(contribution) === true || contribution === undefined) {
+     if (isNaN(contribution) === true || contribution === undefined || contribution < 0) {
   	return (`Параметр <contribution> содержит неправильное значение <${contributionCheck}>`);
   }
    if (isNaN(amount) === true || amount === undefined || amount <= 0) {
   	return (`Параметр <amount> содержит неправильное значение <${amountCheck}>`);
   }
    let currentDate = new Date();
-   let daymSecValue = 1000 * 60 * 60 * 24;
    let defaultAmount = amount - contribution;
-   let numberOfMonths = Math.ceil((((date.getTime() - currentDate.getTime()) / daymSecValue) / 365) * 12);
-   console.log(numberOfMonths);
+   let currentYear = currentDate.getFullYear();
+   let currentMonth = currentDate.getMonth();
+   let loanCloseYear = date.getFullYear();
+   let loanCloseMonth = date.getMonth();
+   if (loanCloseYear < currentYear) {
+   	return (`Параметр <date> содержит неправильное значение <${date.getFullYear()}>`);
+   }
+   if (loanCloseYear === currentYear && loanCloseMonth <= currentMonth) {
+   	return (`Параметр <date> содержит неправильное значение <${date.getMonth() + 1}, ${date.getFullYear()}>`);
+   }
+   if (loanCloseYear === currentYear && loanCloseMonth > currentMonth) {
+   	numberOfMonths = loanCloseMonth - currentMonth;
+   }
+   if (loanCloseYear > currentYear && loanCloseMonth < currentMonth) {
+   	numberOfMonths = (loanCloseYear - currentYear) * 12 + (loanCloseMonth + 1);
+   }
+   if (loanCloseYear > currentYear && loanCloseMonth === currentMonth) {
+    numberOfMonths = (loanCloseYear - currentYear) * 12;
+   }
+  if (loanCloseYear > currentYear && loanCloseMonth > currentMonth) {
+    numberOfMonths = (loanCloseYear - currentYear) * 12 + (loanCloseMonth - currentMonth);
+  }
    let percentRate = percent / (100 * 12);
-   if (numberOfMonths < 1) {
-   	return (`Параметр <date> содержит неправильное значение <${date}>`);
-   } 
-   else { 
-     let payment = defaultAmount * (percentRate + percentRate / ((Math.pow((1 + percentRate), numberOfMonths)) - 1));
-     let totalAmount = payment * numberOfMonths;
-     return Number(totalAmount.toFixed(2));
- }
+   let payment = defaultAmount * (percentRate + percentRate / ((Math.pow((1 + percentRate), numberOfMonths)) - 1));
+   let totalAmount = payment * numberOfMonths;
+
+   return Number(totalAmount.toFixed(2));
 }
 
 
