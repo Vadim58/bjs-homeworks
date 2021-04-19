7f0f90102ee0976b20dfd317249e0181af387033
Vadik <awesome.rdeg@yandex.ru>
2021-04-19 14:19:57 +0300

    Return notation fix

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,19 +1,17 @@
  String.prototype.isPalindrome = function () {
   
   if (this.split(' ').join('').split('').reverse().join('').trim().toLowerCase() === this.split(' ').join('').trim().toLowerCase()) {
-      return String;
+      return true;
   }
 
-  else return !String;
+  else return false;
  
-
 }
 
  
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+   
 }
 
 function checkBirthday(birthday) {
