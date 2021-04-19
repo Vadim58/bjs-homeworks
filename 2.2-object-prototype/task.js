5a2ecd67773b25091304290d46c9f804c928264f
Vadik <awesome.rdeg@yandex.ru>
2021-04-19 14:40:34 +0300

    Second task complete

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -11,11 +11,20 @@
  
 
 function getAverageMark(marks) {
-   
+	let sum = 0;
+	let averageRate = 0;
+	for (i = 0; i < marks.length; i++) {
+		sum += marks[i];
+	}
+	if (sum === 0) {
+		return 0;
+	}
+
+	averageRate = sum / marks.length;
+
+   return Math.round(averageRate);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+   let data = new Date;
 
