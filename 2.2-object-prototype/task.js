e4cd59a7174c157a7164a1820843591ba5d56d66
Vadik <awesome.rdeg@yandex.ru>
2021-04-19 13:58:26 +0300

    first commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,15 @@
-//String.prototype.isPalindrome - для задачи №1
+ String.prototype.isPalindrome = function () {
+  
+  let a = this;
+  let trimmer = a.trim();
+  
+  
+  
+  console.log(a.split(' ').join('').split('').reverse().join('').trim() === a.split(' ').join(''));
+
+}
+
+ 
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
@@ -9,3 +20,4 @@ function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
     // return verdict
 }
+
