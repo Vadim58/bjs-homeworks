d3564daa53ad588df32f7f692fe6d5d7a1feaade
Vadim58 <awesome.rdeg@yandex.ru>
2021-04-20 18:31:14 +0300

    final debug

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -20,13 +20,3 @@ function getAverageMark(marks) {
    return Math.round(averageRate);
 }
 
-function checkBirthday(birthday) {
-   let age;
-   let now = new Date ();
-   let diff = now.getTime() - birthday.getTime(); 
-   console.log(diff);
-   let year = now.getTime() / 1000 / 60 / 60 / 24 / 365;
-   age = diff / year;
-    console.log(age);
-} 
-
