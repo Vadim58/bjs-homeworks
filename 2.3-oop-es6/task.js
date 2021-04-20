32ee2244a262fb7be3655115eef7f67fe98b8072
Vadim58 <awesome.rdeg@yandex.ru>
2021-04-20 21:44:08 +0300

    Very first commit

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -3,11 +3,21 @@ class PrintEditionItem {
      this.name = name;
      this.releaseDate = releaseDate;
      this.pagesCount = pagesCount;
-     this.state = 100;
+     this.state = 1000;
      this.type = null;
 	}
 	fix () {
-		return state * 2;
+		return state * 1.5;
+	}
+	set(state) {
+		if (state < 0) {
+			this.state = 0;
+          }
+        if (state > 100) {
+        	this.state = 100;
+        }
 	}
 	
 }
+const bakunin = new PrintEditionItem ('Анархия', 1909, 389);
+console.log(bakunin.state);
