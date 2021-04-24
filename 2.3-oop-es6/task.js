c090d4be4f8bd3b5442d21d5d60e6007fd592fd0
Vadim58 <awesome.rdeg@yandex.ru>
2021-04-24 13:44:52 +0300

    Second task in work

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,89 +1,101 @@
 class PrintEditionItem {
-	constructor (name, releaseDate, pagesCount){
-     this.name = name;
-     this.releaseDate = releaseDate;
-     this.pagesCount = pagesCount;
+ constructor (name, releaseDate, pagesCount){
+  this.name = name;
+  this.releaseDate = releaseDate;
+  this.pagesCount = pagesCount;
+  this.type = null;
+  this.state = 100;
      }
-     type = null;
-     fix() {
-     	    if (this.state * 1.5 <= 100) {
-           return this.state * 1.5;
-    }
-    else if (this.state * 1.5 > 100) {
-    	return 100;
-    }
+     
+ fix() {
+  if (this.state * 1.5 <= 100) {
+   return this.state * 1.5;
+ }
+  else if (this.state * 1.5 > 100) {
+   	return 100;
+  }
 }
-   set state(arg) {
-              if (arg < 0) {
-              	this._state = 0;
-                }
+ set state(arg) {
+   if (arg < 0) {
+     this._state = 0;
+ }
 
-                if (arg > 100) {
-                	this._state = 100;
-                }
-                if (arg > 0 && arg <= 100) {
-                	this._state = arg;
-                }
-              }
+   if (arg > 100) {
+      this._state = 100;
+ }
+   if (arg > 0 && arg <= 100) {
+      this._state = arg;
+ }
+}
 	
-	get state() {
-		return this._state;
-	}
+  get state() {
+	return this._state;
+ }
 }
 
 const books = new PrintEditionItem ('tolstoy', 1902, 300);
-books.state = 100;
+books.state = 45;
 console.log(books.state);
 console.log(books.fix());
 
 
 class Magazine extends PrintEditionItem {
-	constructor (name, releaseDate, pagesCount) {
-	super (name, releaseDate, pagesCount);
-	this.type = 'magazine';
+ constructor (name, releaseDate, pagesCount) {
+  super (name, releaseDate, pagesCount);
+  this.type = 'magazine';
   }
 }
 
 class Book extends PrintEditionItem {
-	constructor (author, name, releaseDate, pagesCount) {
-      super (name, releaseDate, pagesCount);
-      this.author = author;
-      this.type = 'book';
-	}
+ constructor (author, name, releaseDate, pagesCount) {
+   super (name, releaseDate, pagesCount);
+   this.author = author;
+   this.type = 'book';
+  }
 }
  class NovelBook extends Book {
- 	constructor (name, releaseDate, pagesCount, author) {
- 		super (name, releaseDate, pagesCount, author);
- 		this.type = 'novel';
- 	}
- }
+ constructor (author, name, releaseDate, pagesCount) {
+  super (author, name, releaseDate, pagesCount);
+  this.type = 'novel';
+  }
+}
 
  class FantasticBook extends Book {
- 	constructor (name, releaseDate, pagesCount, author) {
- 		super (name, releaseDate, pagesCount, author);
- 		this.type = 'fantastic';
- 	}
+ constructor (author, name, releaseDate, pagesCount) {
+ 	super (author, name, releaseDate, pagesCount);
+ 	this.type = 'fantastic';
  }
+}
  class DetectiveBook extends Book {
- 	constructor (name, releaseDate, pagesCount, author) {
- 		super (name, releaseDate, pagesCount, author);
- 		this.type = 'detective';
- 	}
+  constructor (author, name, releaseDate, pagesCount) {
+ 	super (author, name, releaseDate, pagesCount);
+ 	this.type = 'detective';
  }
+}
  
 
  class Library {
- 	constructor (name) {
- 		this.name = name;
- 	}
- 	books = [];
- 	addBook(book) {
- 		if (book.state > 30) {
- 			this.books.push(book);
- 		}
-
- 	}
-   
+  constructor (name) {
+ 	this.name = name;
+    this.books = [];
+   } 	
+ 	addBook(book) {	
+     if (book.state > 30) {
+     	this.books.push(book);
+     }
+   }
+    findBookBy (type, value) {
+     for (let i = 0; i < this.books.length; i++) {
+      if (this.books[i].type === value) {
+    		return this.books[i];
+    }
+   	else {
+    return null;
+   }
+  }
  }
+   
+}
 
- const library = new Library("Библиотека имени Ленина");
+
+
