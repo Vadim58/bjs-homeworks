6f05c6bd79669a05150e0307ab569013b387e8da
Vadim58 <awesome.rdeg@yandex.ru>
2021-05-04 14:30:59 +0300

    Critical fix for first task

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -1,40 +1,21 @@
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 function getNames () {
-  let weaponName = [];
-  for (let i = 0; i < weapons.length; i++) {
-    weaponName.push(weapons[i].name);
-  }
-  return weaponName;
+ return weapons.map(weapon => weapon.name);
 }
+
 function getCountReliableWeapons(durabilityValue) {
-  let numberOfReliableWeapons = 0;
-   for (let i = 0; i < weapons.length; i++) {
-     if (weapons[i].durability > durabilityValue) {
-       numberOfReliableWeapons++;
-    }       
-  }
-  return numberOfReliableWeapons;
+  return weapons.filter(weapon => weapon.durability > durabilityValue).length;
 }
+
 function hasReliableWeapons(durabilityValue) {
-  for (let i = 0; i < weapons.length; i++) {
-   	if (weapons[i].durability > durabilityValue) {
-      return true;
-    }
-  }
+ return weapons.some(weapon => weapon.durability > durabilityValue);
 }
+
+
 function getReliableWeaponsNames(durabilityValue) {
-  let reliableWeaponsNames = [];
-    for (let i = 0; i < weapons.length; i++) {
-   	  if (weapons[i].durability > durabilityValue) {
-        reliableWeaponsNames.push(weapons[i].name); 
-     }
-   }
-  return reliableWeaponsNames;
+ return weapons.filter(weapon => weapon.durability > durabilityValue).map(relibleWeapon => relibleWeapon.name);
 }
+
 function getTotalDamage() {
-  let totalDamage = 0;
-    for (let i = 0; i < weapons.length; i++) {
-      totalDamage += weapons[i].attack;
-  }
-  return totalDamage;
+  return weapons.reduce((totalDamage, weapon) => totalDamage + weapon.attack, 0);
 }
