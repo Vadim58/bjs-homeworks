8dc235d1873ccdd22fde1e1ed86409ea0ac6cdcd
Vadim58 <awesome.rdeg@yandex.ru>
2021-05-14 18:18:09 +0300

    Gloryous

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,33 +1,22 @@
-const compareArrays = (arr1, arr2) => {if (arr1.length === arr2.length && ''+arr1 === ''+arr2)
- return true; 
- else {
- 	return false
- } 
-}
+const compareArrays = (arr1, arr2) => {
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])
+};
 
-function memorize(fn,limit) {
-	let memory = [
-  {
-    args: [3, 4],
-    result: 7
-  },
-  {
-    args: [1, 3],
-    result: 4
-  }
-];
-const findObject = (arr) => memory.find(object => compareArrays(object.args, arr)); 
- return (...someArgs) => { return findObject([...someArgs]).result;    	  	  
-	  	{memory.push(
- 	  	     {
- 	  		  args: [...someArgs],
- 	  	      result: fn(...someArgs),
- 	  	    }
- 	  	 )
- 	  	memory.splice(limit - 1);
- 	  	return fn(...someArgs); 
-  }
- }
+function memorize(fn, limit) {
+    let memory = [];
+    return (...someArgs) => {
+        const findedObject = memory.find(object => compareArrays(object.args, someArgs));
+         if (findedObject) {
+            return findedObject.result
+        };
+        const res = fn(...someArgs);
+        memory.push({
+            args: someArgs,
+            result: res,
+        })
+        memory.splice(limit - 1);
+        return res;
+    }
 }
-const resultFunction = memorize(((a,b) => a + b), 23);
-resultFunction(3,4);
+const resultFunction = memorize(((a, b) => a + b), 23);
+resultFunction(3, 4);
